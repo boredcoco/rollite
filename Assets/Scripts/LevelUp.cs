cd6bab0cce74f737a46cd06adafd71a13b3cc42d
Coco <chuaziyifaith@gmail.com>
2022-07-14 09:08:51 +0800

    Level Up implemented for antagonist
    - antagonist speed changes

---
+++ Assets/Scripts/LevelUp.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUp : MonoBehaviour
+{
+    [SerializeField] private float popUpTime = 0.1f;
+
+    private float timer;
+
+    private void Start()
+    {
+      timer = popUpTime;
+    }
+
+    private void Update()
+    {
+      if (timer <= 0f)
+      {
+        gameObject.SetActive(false);
+      } else
+      {
+        timer -= Time.deltaTime;
+      }
+    }
+}
