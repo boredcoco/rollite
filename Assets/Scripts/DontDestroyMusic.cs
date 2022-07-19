397879332d4e3068b4865481d067074fb6fe80cd
Coco <chuaziyifaith@gmail.com>
2022-07-20 00:58:29 +0800

    One single bgm object for entire game
    - better version of previous commit where all bgms are handled by one object throughout the entire game

--- Assets/Scripts/DontDestroyMusic.cs
+++ Assets/Scripts/DontDestroyMusic.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class DontDestroyMusic : MonoBehaviour
 {
+    [SerializeField] private AudioSource mainMenu_bgm;
+    [SerializeField] private AudioSource littleEngine;
+    [SerializeField] private AudioSource glitchBot;
+    [SerializeField] private AudioSource spaceVoyager;
+    [SerializeField] private AudioSource coldPlanet;
+
+    private AudioSource current;
+
     private void Awake()
     {
-      GameObject[] obj = GameObject.FindGameObjectsWithTag("Music");
-      if (obj.Length > 1)
+      GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+      if (objs.Length > 1)
       {
         Destroy(this.gameObject);
       } else
@@ -22,7 +30,37 @@ public class DontDestroyMusic : MonoBehaviour
       if (SceneManager.GetActiveScene().name == "Base"
       || SceneManager.GetActiveScene().name == "SinglePlayer Base")
       {
-        Destroy(this.gameObject);
+        if (!(current == littleEngine || current == glitchBot || current == spaceVoyager))
+        {
+          current.Stop();
+          if (SettingsManager.BGMNum == 1) current = littleEngine;
+          else if (SettingsManager.BGMNum == 2) current = glitchBot;
+          else if (SettingsManager.BGMNum == 3) current = spaceVoyager;
+
+          current.Play();
+        }
+      } else if (SceneManager.GetActiveScene().name == "Main Menu")
+      {
+        if (current == null)
+        {
+          current = mainMenu_bgm;
+          current.Play();
+        } else if (current != mainMenu_bgm)
+        {
+          current.Stop();
+          current = mainMenu_bgm;
+          current.Play();
+        }
+      } else if (SceneManager.GetActiveScene().name == "SP Retry"
+      || SceneManager.GetActiveScene().name == "MP Retry")
+      {
+        if (current != coldPlanet)
+        {
+          current.Stop();
+          current = coldPlanet;
+          current.Play();
+        }
       }
+
     }
 }
