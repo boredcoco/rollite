10ef972f3b6982dfbb60ff6ac4655cb4513a7d04
Coco <chuaziyifaith@gmail.com>
2022-07-24 12:48:03 +0800

    Revert "Revert "Merge pull request #15 from boredcoco/Error-corrections""
    This reverts commit 9a6312008b4100e37ae807e3b2c08ca2b7bfbb54.

--- Assets/Scripts/Instructions/Instructions.cs
+++ Assets/Scripts/Instructions/Instructions.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class Instructions : MonoBehaviour
 {
-    [SerializeField] private string movement = "Use WASD keys to Move Plane";
+    [SerializeField] private string movement = "Use WASD keys to Move Plane. Note that there is a screen-wrap at all edges of this game. So if you are at the left screen boundary you will be teleported to the right, vice versa. This applies to top and bottom screen boundaries. Try going to each edge to see what happens!";
     [SerializeField] private string dashing = "Dodge attacks launched at you, or press the 'space' key to dash through attacks. Remember that dahsing depletes Player Stamina, which will regenerate over time.";
     [SerializeField] private string buffs = "Buffs are power-ups that spawn randomly on the map. They activate when you come into contact with them. Each buff has a different effect on you and your opponent. Try activating them to see the effects.";
     [SerializeField] private string antagonist = "In SinglePlayer mode, the Antagonist will spawn attacks and try to get as close to you as it can. In Multiplayer mode, your friend does the attacking, click on the left side panel to select an attack. Once selected, click on where you would like to place the attack on the map.";
@@ -25,19 +25,24 @@ public class Instructions : MonoBehaviour
 
     public void next()
     {
-      if (index < allWords.Length - 1)
+      if (index >= allWords.Length - 1)
+      {
+        index = 0;
+      } else
       {
         index++;
-        txt.text = allWords[index];
       }
+      txt.text = allWords[index];
     }
 
     public void back()
     {
-      if (index != 0)
+      if (index > 0)
       {
         index--;
-        txt.text = allWords[index];
+      } else {
+        index = allWords.Length - 1;
       }
+      txt.text = allWords[index];
     }
 }
