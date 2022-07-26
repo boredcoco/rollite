5e9b0bb64ee5791a7087b2181e8472e8fc74b5b9
nickeltea <97277251+nickeltea@users.noreply.github.com>
2022-07-27 03:57:34 +0800

    Added script to Main Menu to set up gameplay according to settings

---
+++ Assets/Scripts/Settings Scripts/Setup.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Setup : MonoBehaviour
+{
+    private int colour;
+    private int speed;
+
+    [SerializeField] public static Color firstColour = new Color(0.6992524f, 0.8516356f, 0.8679245f, 1);
+    [SerializeField] public static Color secondColour = new Color(0.8784314f, 0.4980392f, 0.6039216f, 1);
+    [SerializeField] public static Color thirdColour = new Color(0.1960784f, 0.1921569f, 0.4666667f, 1);
+
+    [SerializeField] private Sprite bluePlane;
+    [SerializeField] private Sprite pinkPlane;
+    [SerializeField] private Sprite purplePlane;
+
+    public AudioMixer audioMixer;
+
+    private void Awake()
+    {
+        colour = PlayerPrefs.GetInt("colour", 1);
+        speed = PlayerPrefs.GetInt("speed", 2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setup Colour
+        if (colour == 1)
+        {
+            PlaneColour.defaultColour = bluePlane;
+            BGColour.background = firstColour;
+        } else if (colour == 2)
+        {
+            PlaneColour.defaultColour = pinkPlane;
+            BGColour.background = secondColour;
+        } else
+        {
+            PlaneColour.defaultColour = purplePlane;
+            BGColour.background = thirdColour;
+        }
+
+        // Setup Speed
+        if (speed == 1)
+        {
+            BasicMovement.speed = 6f;
+            BasicMovement.dashForce = 10f;
+        }
+        else if (speed == 2)
+        {
+            BasicMovement.speed = 8f;
+            BasicMovement.dashForce = 15f;
+        }
+        else
+        {
+            BasicMovement.speed = 12f;
+            BasicMovement.dashForce = 20f;
+        }
+
+        // Setup BG Music
+        audioMixer.SetFloat("BGM", 30f * Mathf.Log10(PlayerPrefs.GetFloat("BGMvol", 0.5f)));
+
+        // Setup SFX Music
+        audioMixer.SetFloat("Sound Effects", 30f * Mathf.Log10(PlayerPrefs.GetFloat("SFXvol", 0.5f)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
