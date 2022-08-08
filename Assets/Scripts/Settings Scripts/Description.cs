7b7aa60fb0b84428ee506d23116a6242d7a5754c
Coco <chuaziyifaith@gmail.com>
2022-08-08 15:48:32 +0800

    Update Description.cs

--- Assets/Scripts/Settings Scripts/Description.cs
+++ Assets/Scripts/Settings Scripts/Description.cs
@@ -14,14 +14,13 @@ public class Description : MonoBehaviour
     {
         txt = GetComponent<TMP_Text>();
         defaultText = txt.text;
-        
-        if (about == 1) txt.text = defaultText + " " + MapSettings.obsNum.ToString();
-        if (about == 2) txt.text = defaultText + " " + MapSettings.obs_orientation;
+
+        if (about == 1) txt.text = defaultText + " " + PlayerPrefs.GetInt("obstacles", 3).ToString();
+        if (about == 2) txt.text = defaultText + " " + PlayerPrefs.GetString("orientation", "Random");
 
         if (about == 3)
         {
-          if (MapSettings.have_obstacles) txt.text = defaultText + " Yes";
-          else txt.text = defaultText + " No";
+          txt.text = defaultText + " " + PlayerPrefs.GetString("haveObstacles", "Yes");
         }
     }
 
