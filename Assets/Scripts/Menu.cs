e920b59b317ac61657d3774b08c35f5c63055c31
Coco <chuaziyifaith@gmail.com>
2022-08-08 19:06:09 +0800

    added click sounds to main menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,11 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     public TextMeshProUGUI singleplayerScore;
     public TextMeshProUGUI multiplayerScore;
+
+    [SerializeField] private AudioSource clickSound;
 
     void Start()
     {
@@ -15,31 +18,37 @@ public class Menu : MonoBehaviour
 
     public void loadMultiplayer()
     {
+        clickSound.Play();
         SceneManager.LoadScene("Base");
     }
 
     public void loadSingleplayer()
     {
+        clickSound.Play();
         SceneManager.LoadScene("SinglePlayer Base");
     }
 
     public void settings()
     {
+        clickSound.Play();
         SceneManager.LoadScene("Settings");
     }
 
     public void loadMapMenu()
     {
+      clickSound.Play();
       SceneManager.LoadScene("Map Settings");
     }
 
     public void loadInstructions()
     {
+        clickSound.Play();
         SceneManager.LoadScene("Instructions");
     }
 
     public void resetHighscores()
     {
+        clickSound.Play();
         PlayerPrefs.SetInt("SP Score", 0);
         PlayerPrefs.SetInt("MP Score", 0);
 
